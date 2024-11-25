99ce9eabf00e492faf1de5ae3d3bf60fe62479d7
tabib-e-alahi <etabib.alahi@gmail.com>
2024-11-26 00:32:41 +0600

    size and max_size string built in functions added

---
+++ String in c++/String built_in funtions/capacity_functions/max_size().cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello world";
+    // it returns the maximum size that string can hold, can vary device to device
+    cout << s.max_size() << endl;
+    return 0;
+}
+
---
+++ String in c++/String built_in funtions/capacity_functions/size().cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello world";
+    // it returns the size of the string s
+    cout << s.size() << endl;
+    return 0;
+}
