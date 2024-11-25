3e316516e20bed472f9d4d41c1c222469fb2be14
tabib-e-alahi <etabib.alahi@gmail.com>
2024-11-26 00:35:34 +0600

    capacity() and clear() string built in functions added

---
+++ String in c++/String built_in funtions/capacity_functions/capacity().cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello worldw alahi tabib";
+    // it returns the currect available capacity of the string, dynamically changed
+    cout << s.capacity() << endl;
+    return 0;
+}
+
+
---
+++ String in c++/String built_in funtions/capacity_functions/clear().cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello worldw alahi tabib";
+    // it clear the string
+    s.clear();
+    cout << s << endl;
+    return 0;
+}
+
+
+
